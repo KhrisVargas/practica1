5244def77d480e3b0110d7ccb84a4e2bc2a72d6d
KhrisVargas <khristian.jp@gmail.com>
2022-08-06 20:15:34 -0500

    Es el segundo ejercicio

---
+++ Ejercicio2.java
@@ -0,0 +1,8 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        String primerApellido="Paucar";
+        String segundoApellido="Vargas";
+
+        System.out.println("Mi Apellido es " + primerApellido + " " + segundoApellido + ".");
+    }
+}
