db63ab5fb994a8b30841a0735a36c40de37f9c23
KhrisVargas <110554198+KhrisVargas@users.noreply.github.com>
2022-08-06 19:58:13 -0500

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,8 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String primerNombre="Cristian";
+        String segundoNombre="Miguel";
+
+        System.out.println("Mi Nombre es " + primerNombre + " " + segundoNombre + ".");
+    }
+}
